fb405c9375c6f8d27e99a5f5854cec06428291a4
nyx <nyx@artyfetes.com>
2025-05-27 14:25:30 +0200

    debug false

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def afficher_personnes():
 # La route /metrics est gérée automatiquement par PrometheusMetrics
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
